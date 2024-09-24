e11868f8ff9fc5a589632ffbba26fc2000e06855
Ahmed S. <s.ahmed.profiquence@gmail.com>
2024-09-24 20:27:33 +0530

    User Schema Defined

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['attendee', 'organizer'],
+        default: 'attendee'
+    }
+});
+
+export default model('User', userSchema);
