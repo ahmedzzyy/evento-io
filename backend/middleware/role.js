6f86f58fb6677db11eb975267fb4372662962ecc
Ahmed S. <s.ahmed.profiquence@gmail.com>
2024-10-07 12:33:06 +0530

    Full User data fetch instead of just receiving _id

--- backend/middleware/role.js
+++ backend/middleware/role.js
@@ -1,5 +1,8 @@
-const checkRole = (roles) => (req, res, next) => {
-    if (!roles.include(req.user.role)) {
+import User from "../models/User.js";
+
+const checkRole = (roles) => async (req, res, next) => {
+    const user = await User.findById(req.user.id).select('-password');
+    if (!roles.includes(user.role)) {
         return res.status(403).json(
             { msg: 'Access denied: You do not have permission' }
         );
