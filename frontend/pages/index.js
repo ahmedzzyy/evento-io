68eba16a736e119c9d0be9e7ffe6ee5c16a410cb
Ahmed S. <s.ahmed.profiquence@gmail.com>
2024-10-08 09:54:47 +0530

    Cleaned Homepage UI

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -37,17 +37,14 @@ const HomePage = () => {
     }
 
     return (
-        <div className="homepage">
+        <div className="container">
             <h1>Welcome to Evento!</h1>
             <p>Explore upcoming events or create your own!</p>
 
             <div className="event-list">
                 {events.length > 0 ? (
                     events.map(event => (
-                        <>
-                            <EventCard key={event._id} event={event} />
-                            <Link to={`/events/${event._id}`}>View Details</Link>
-                        </>
+                        <EventCard key={event._id} event={event} />
                     ))
                 ) : (
                     <p>No upcoming events available</p>
